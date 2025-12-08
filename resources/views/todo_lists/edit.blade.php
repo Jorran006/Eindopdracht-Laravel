7518070ba999c56a94b9f025c2a0ed9269a6238d
Jorran006 <Jorranderooij@gmail.com>
2025-12-08 22:53:55 +0100

    lijst verwijderen knop toegevoegd aan edit

--- resources/views/todo_lists/edit.blade.php
+++ resources/views/todo_lists/edit.blade.php
@@ -1,16 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Nieuwe lijst maken</h1>
+<h1>Lijst bewerken: {{ $todoList->title }}</h1>
 
-<form action="{{ route('todo_lists.store') }}" method="POST">
+<form action="{{ route('todo_lists.update', $todoList->id) }}" method="POST">
     @csrf
+    @method('PUT')
+
     <label>Titel:</label>
-    <input type="text" name="title">
+    <input type="text" name="title" value="{{ $todoList->title }}">
+
     <label>Beschrijving:</label>
-    <textarea name="description"></textarea>
-    <button type="submit">Opslaan</button>
+    <textarea name="description">{{ $todoList->description }}</textarea>
+
+    <button type="submit">Wijzigen</button>
 </form>
 
-<a href="{{ route('todo_lists.index') }}">Terug</a>
+<form action="{{ route('todo_lists.destroy', $todoList->id) }}" method="POST" onsubmit="return confirm('Weet je zeker dat je deze lijst wilt verwijderen?');">
+    @csrf
+    @method('DELETE')
+    <button type="submit">Verwijderen</button>
+</form>
+
+<p><a href="{{ route('todo_lists.index') }}">Terug</a></p>
 @endsection
